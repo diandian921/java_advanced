ce44300914c872c7364fd5ca6ca30fa4d6b608ba
xiaowu <diandian921@gmail.com>
2021-07-04 11:41:52 +0800

    week02

---
+++ src/main/java/week02/HttpClientTest.java
@@ -0,0 +1,24 @@
+/*
+ * Alipay.com Inc.
+ * Copyright (c) 2004-2021 All Rights Reserved.
+ */
+package week02;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author wuwh
+ * @version HttpClientTest.java, v 0.1 2021年06月28日 14:42 wuwh
+ */
+public class HttpClientTest {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger("DEFAULT");
+
+    public static void main(String[] args) {
+        String url = "http://localhost:8801";
+        String result = HttpClientUtil.get(url);
+        LOGGER.info("请求{},返回:{}", url, result);
+    }
+
+}
